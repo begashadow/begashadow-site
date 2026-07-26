0d8ef7111526aa3e633b665ebebe4f4e47783f9e
BEGA Shadow Resistance <j.lambilliotte@pm.me>
2026-07-26 22:58:01 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener("load", () => {
-  window.setTimeout(() => {
-    document.getElementById("intro").classList.add("hide");
-  }, 3000);
-});
-
-document.getElementById("year").textContent = new Date().getFullYear();
